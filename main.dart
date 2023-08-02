ad46ddeae3d27988420bc6ea397e678d5604ab60
Sakurah-ismail <81982746+Sakurah-ismail@users.noreply.github.com>
2023-08-02 21:26:53 +0600

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home:Center(child:Text ('Hello World'),),));
+ print("Hello World");
+ runApp(
+   MaterialApp(
+     home:Scaffold(
+         body: Center(
+             child: Text("Hello World")
+         )
+     )
+   )
+ );
 }
